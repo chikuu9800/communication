52e2b0da8c98fb3f716e93685619b7439e08d9b1
chikuu9800 <shubhamchikhale40@gmail.com>
2025-12-01 14:47:31 +0530

    mongodb

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,9 +1,35 @@
+// // config/mongo.js
+// import mongoose from "mongoose";
+
+// const connectMongoDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/ICAI_DB");
+
+//     console.log("✅ Connected to MongoDB successfully!");
+//   } catch (error) {
+//     console.error("❌ MongoDB connection error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectMongoDB;
+
 // config/mongo.js
 import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ICAI_DB");
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      console.error("❌ ERROR: MONGO_URI is missing in Railway Variables");
+      process.exit(1);
+    }
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("✅ Connected to MongoDB successfully!");
   } catch (error) {
@@ -13,4 +39,3 @@ const connectMongoDB = async () => {
 };
 
 export default connectMongoDB;
-
